2a3fa184bb5d89a8595a321b26c07ffe823a1aae
VenkataDeepika Thulasi <venkatadeepikathulasi@gmail.com>
2025-04-11 23:11:54 +0530

    Basic Form

---
+++ BasicForm/src/App.js
@@ -0,0 +1,10 @@
+import "./index.css";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+const Form = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+};
+export default Form;
